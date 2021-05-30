0a16ce8866b4382ea8575e9e74a423b8a8d35576
Adarsh Raj <67409491+coderscoding@users.noreply.github.com>
2021-05-30 19:52:20 +0530

    removed some comments

--- main.py
+++ main.py
@@ -47,33 +47,21 @@ def product_data_save():
         # secure filename for on space and no error while saving
         fileSeries = 0
         cursor.execute("SELECT * FROM `products__info`")
-        ## this will fetch only one from starting
-        # product_info_data = cursor.fetchone()
-        # # this will all 
         product_info_data = cursor.fetchall()
     
-        # # it was for only one fetchone
-        # if product_info_data != None:
-        #     print(product_info_data)
-        #     print('fdgfd')
-        # # it is for all
+
         if product_info_data != ():
             print(product_info_data)
             # product_info_data
             fileSeries = len(product_info_data)
             fileSeries+=1
-            # for sd in product_info_data:
-            #     # str(sd).index()
-            #     print(str(sd))
         
         procductFileName = 'static/products/'+secure_filename(f'productImage_{str(fileSeries)}')
         cursor.execute(f"INSERT INTO `products__info` (`Sn.`, `Products Name`, `Description`, `Price`, `Filename`) VALUES (NULL, '{prductName}', '{productDescription}', '{priceProduct}', '{procductFileName}');")
-        # data = cursor.fetchone()
-        # #saving files to product file names
+
         file_form.save(procductFileName)
         session['productAddedMsg'] = 'productadded'
         return redirect(url_for('admin'))
-        # return "data"
     else:
         return "Get Method Not Allowed"
 
@@ -86,13 +74,11 @@ def login():
     elif 'logoutSuccessfully' in session:
         session['logoutSuccessfully'] = ''
         vari = 'Logout Successfully'
-        # and clearing all here to logout
         app.secret_key = 'SeSsIoNkEyFoRLoginR'
         session.clear()
         return render_template('login.html', vari=vari)
     elif 'wrongPass' in session:
         print("ehree")
-        # session['wrongPass'] = 'DothisStuff'
         session.clear()
         return render_template('login.html', vari="Wrong Username and Password Combination. Try Again")
     else:
@@ -103,11 +89,8 @@ def login():
 def checkLogin():
     data = request.form
     print(data['username'])
-    # for now I'm only testing no database iahev used here
     if data['username'] == 'admin':
-        # firstly clear all other session
         session.clear()
-        # then creating a new session
         session['admin'] = data['username']
         return redirect("/admin")
     else:
@@ -118,7 +101,6 @@ def checkLogin():
 @app.route("/adminsignout")
 def adminSignOut():
     if 'admin' in session:
-        # i am not clearing all sessoin because i need it to clean two times when logout msg will show after that
         session.clear()
         session['logoutSuccessfully'] = 'logoutSuccessfully'
         return redirect(url_for('login'))
@@ -140,33 +122,9 @@ if __name__ == '__main__':
     app.config['MYSQL_DATABASE_DB'] = "mydatabase"
     myMysqlApp.init_app(app)
     app.secret_key = 'SeSsIoNkEyFoRLogin'
-    # app configuration
     app.config['SESSION_TYPE'] = 'login'
-    # # it's configuration for upload file
     app.config['UPLOAD_FOLDER'] = 'static/products'
-    # maximum size of uploading file
     app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
 
     app.run(debug=True)
-    # Quick test configuration. Please use proper Flask configuration options
-    # in production settings, and use a separate file or environment variables
-    # to manage the secret key!
-
-#     from datetime import timedelta
-# from flask import session, app
-
-# @app.before_request
-# def make_session_permanent():
-#     session.permanent = True
-#     app.permanent_session_lifetime = timedelta(minutes=5)
-
-    # for uploaded_file in request.files.getlist('file'):
-    #     if uploaded_file.filename != '':
-    #         uploaded_file.save(uploaded_file.filename)
-
-# @app.route('/', methods=['POST'])
-# def upload_file():
-#     uploaded_file = request.files['file']
-#     if uploaded_file.filename != '':
-#         uploaded_file.save(uploaded_file.filename)
-#     return redirect(url_for('index'))
+ 
